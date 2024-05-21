6dd3af99db2df60a4d8540e502385cc59421ccb1
Marshall Garey <marshall@schedmd.com>
2024-05-21 17:34:08 -0600

    Provision with --debug and restart the VMs after

--- ubuntunew/provision.sh
+++ ubuntunew/provision.sh
@@ -8,9 +8,12 @@
 #vagrant provision node1 | tee vagrant_provision_node1.log &
 #vagrant provision node2 | tee vagrant_provision_node2.log &
 
-vagrant up --provision node0  | tee vagrant_provision_node0.log &
-vagrant up --provision node1  | tee vagrant_provision_node1.log &
-vagrant up --provision node2  | tee vagrant_provision_node2.log &
-vagrant up --provision ctld  | tee vagrant_provision_ctld.log &
-vagrant up --provision dbd  | tee vagrant_provision_dbd.log &
+vagrant up --provision --debug node0  | tee vagrant_provision_node0.log &
+vagrant up --provision --debug node1  | tee vagrant_provision_node1.log &
+vagrant up --provision --debug node2  | tee vagrant_provision_node2.log &
+vagrant up --provision --debug ctld  | tee vagrant_provision_ctld.log &
+vagrant up --provision --debug dbd  | tee vagrant_provision_dbd.log &
 wait
+# The nfs mount requires a restart for some reason
+vagrant halt
+vagrant up
