81fa6a3d7332f0fb60d2aa0a81486fc7319247c3
Marshall Garey <marshall@schedmd.com>
2024-05-21 17:34:48 -0600

    WIP - Set things up after the VMs are provisioned

--- ubuntunew/24.05/installfiles/init.sh
+++ ubuntunew/24.05/installfiles/init.sh
@@ -1,72 +1,119 @@
 #!/bin/bash
 
-source init.conf
-source script_common.sh
-
 ###############################################################################
 # Functions
 ###############################################################################
-
-function mkenvrc()
+function get_scripts()
 {
-	envrc=".envrc"
-	# envrc used to be a symlink. It isn't now. This rm will remove
-	# the old symlink and ensure that it is a regular file.
-	# This only matters for old setups that still have the symlink.
-	if [ $(find -maxdepth 1 -name "${envrc}" | wc -l) -eq 1 ]
-	then
-		rm "${envrc}"
-	fi
-	echo '# .envrc
-# This file is used by direnv (https://direnv.net/).
-# direnv needs to be hooked into the shell: https://direnv.net/docs/hook.html
-# Once direnv is hooked into the shell, it automatically sets environment
-# variables listed in .envrc.
-SYSCONF=$(pwd)
-export PATH=$SYSCONF/bin:$SYSCONF/sbin:$PATH
-export MANPATH=$SYSCONF/share/man:$MANPATH
-export SACCT_FORMAT="cluster,jobid,jobname%20,state,exitcode,submit,start,end,elapsed,eligible"
-export SPRIO_FORMAT="%.15i %9r %.10Y %.10S %.10A %.10B %.10F %.10J %.10P %.10Q %30T"
-export SLURMRESTD=$(which slurmrestd)' > "${envrc}"
+	slurmsrc="${script_path}/../slurm"
+	cp "${slurmsrc}/etc/burst_buffer.lua.example" "${script_path}/tmpetc/burst_buffer.lua"
+	cp "${slurmsrc}/etc/cli_filter.lua.example" "${script_path}/tmpetc/cli_filter.lua"
+	cp "${slurmsrc}/etc/job_submit.lua.example" "${script_path}/tmpetc/job_submit.lua"
 }
 
-function mkslurmdbd_conf()
+function scp_ssh_key()
 {
+	set -e
+	edkey_name="id_ed25519"
+	edkey_priv="/etc/ssh/ssh_host_${edkey_name}"
+	edkey_pub="/etc/ssh/ssh_host_${edkey_name}.pub"
+	local_edkey_priv="/home/vagrant/.ssh/${edkey_name}"
+	local_edkey_pub="/home/vagrant/.ssh/${edkey_name}.pub"
+	# Generate ssh keys on ctld that do not already exist
+	# TODO: Do the same thing for root as well as vagrant
+	nohup vagrant ssh "ctld" -c '
+		edkey_name="id_ed25519"
+		cd /home/vagrant/.ssh
+		found_key=$(find . -name ${edkey_name} | wc -l)
+		echo ${found_key}
+		if [[ ${found_key} == 0 ]]; then
+			ssh-keygen -t ed25519 -N '' -f ${edkey_name}
+			cat ${edkey_name}.pub >> authorized_keys
+		fi
+		'
+
+	# Copy back to host machine, then distribute to all VMs
+	# This avoids needing a password, which I would need to use ssh-copy-id
+	vagrant scp "ctld:${local_edkey_priv}" "${script_path}"
+	vagrant scp "ctld:${local_edkey_pub}" "${script_path}"
+	pub_out="$(cat ${edkey_name}.pub)"
+
+	for n in ${all_nodes[@]}; do
+		if [[ "${n}" != "ctld" ]]; then
+			# Copy the ctld key to all other nodes
+			vagrant scp "${edkey_name}" "${n}:${local_edkey_priv}"
+			vagrant scp "${edkey_name}.pub" "${n}:${local_edkey_pub}"
+		fi
+		nohup vagrant ssh "${n}" -c "
+			cd /home/vagrant/.ssh
+			echo ${pub_out}
+			if [[ $(grep \"${pub_out}\" authorized_keys | wc -l) == 0 ]]; then
+				echo ${pub_out} >> authorized_keys
+			fi
+			"
+	done
+	# TODO: Add to each node to each other's known_hosts file
+}
+
+function scp_etc()
+{
+	for n in ${ctl_dbd_nodes[@]}; do
+		nohup vagrant ssh "${n}" -c 'cd /home/vagrant; mkdir -p tmpetc'
+	done
+	for f in $(ls tmpetc); do
+		for n in ${ctl_dbd_nodes[@]}; do
+			vagrant scp "${script_path}/tmpetc/${f}" "${n}:/home/vagrant/tmpetc"
+		done
+	done
+}
+
+function ensure_correct_slurm_permissions()
+{
+	# * Set the permissions of slurmdbd.conf and jwt key to 600
+	for n in ${ctl_dbd_nodes[@]}; do
+		nohup vagrant ssh "${n}" -c '
+		cd /home/vagrant
+		sudo rm -rf /usr/local/etc
+		sudo mv ~/tmpetc /usr/local/etc
+		sudo chown slurm:slurm /usr/local/etc
+		sudo chown slurm:slurm /usr/local/etc/*
+		sudo chmod 0600 /usr/local/etc/slurmdbd.conf
+		sudo chmod 0600 /usr/local/etc/jwt_hs256.key
+		'
+	done
 }
 
 ###############################################################################
 # Script start
 ###############################################################################
 
+ctl_dbd_nodes=("ctld" "dbd")
+all_nodes=("ctld" "dbd" "node0" "node1" "node2")
+script_path="$(dirname -- "$( readlink -f -- "$0"; )";)"
+
+set -x
+
 # TODO:
 # * Copy example scripts from slurm/etc/ to tmpetc
-script_path="$(dirname -- "$( readlink -f -- "$0"; )";)"
-slurmsrc="${script_path}/../slurm"
-cp "${slurmsrc}/etc/burst_buffer.lua.example ${script_path}/tmpscripts/burst_buffer.lua"
-cp "${slurmsrc}/etc/cli_filter.lua.example ${script_path}/tmpscripts/cli_filter.lua"
-cp "${slurmsrc}/etc/job_submit.lua.example ${script_path}/tmpscripts/job_submit.lua"
+#get_scripts
 
-# * vagrant scp the config files and scripts to /opt/slurm/etc on the
-# destination ctld/dbd
+# Add my ssh key to all the nodes
+#scp_ssh_key
+
+# * vagrant scp the config files and scripts to user vagrant's home directory.
+# /usr/local/etc on the destination ctld/dbd
 # vagrant scp:
 # Usage: vagrant scp <local_path> [vm_name]:<remote_path>
 #        vagrant scp [vm_name]:<remote_path> <local_path>
+# TODO: vagrant scp does not have -r, so we have to scp each file
+# individually. It would be easier to add a ssh key from my native box to each
+# vm and then do normal scp.
 #
-vagrant scp "${script_path}/tmpetc/*" "ctld:/opt/slurm/etc/"
-vagrant scp "${script_path}/tmpetc/*" "dbd:/opt/slurm/etc/"
-# * Change the permissions of slurmdbd.conf to 600
-nohup vagrant ssh ctld -c 'chmod /opt/slurm/etc/slurmdbd.conf 0600'
-nohup vagrant ssh dbd -c 'chmod /opt/slurm/etc/slurmdbd.conf 0600'
-# TODO: Create a jwt key and set permissions to 600
+#scp_etc
 
-# * Build Slurm on the ctld host:
-nohup vagrant ssh ctld -c '
-cd /opt/slurm/24.05
-mkdir -p build
-cd build
-../../slurm/configure --enable-developer --disable-optimizations --enable-memory-leak-debug
-make.py
-'
+#ensure_correct_slurm_permissions
+
+# * Build Slurm on the ctld host,
 # * scp the resulting Slurm binaries and libraries
 #     (/usr/local/bin and /usr/local/lib) to dbd and all nodes
 # * vagrant scp the systemd service files (slurmd.service, slurmctld.service,
@@ -75,3 +122,27 @@ make.py
 #   - For now, nodes 0 and 1 are compute nodes and node 2 is a login node.
 # * On each compute and login node, set SLURMD_OPTIONS in /etc/default/slurmd
 #   to use the --conf-server option.
+nohup vagrant ssh ctld -c '
+# BUILD:
+cd /opt/slurm
+sudo mkdir -p build
+sudo chown vagrant:vagrant build
+cd build
+#../24.05/slurm/configure --enable-developer --disable-optimizations --enable-memory-leak-debug --with-systemdsystemunitdir=/etc/systemd/system
+#sudo make.py --with-extra etc
+# Distribute the resulting libraries, binaries, and service files to other nodes
+clusternodes=("node0" "node1" "node2" "dbd")
+servicedir="/etc/systemd/system"
+for n in ${clusternodes[@]}; do
+	scp -r /usr/local/lib "vagrant@${n}:/usr/local/"
+	scp -r /usr/local/bin "vagrant@${n}:/usr/local/"
+	scp -r /usr/local/sbin "vagrant@${n}:/usr/local/"
+	scp "${servicedir}/sackd.service" "vagrant@ctld:${servicedir}"
+done
+sudo scp "${servicedir}/slurmdbd.service" "vagrant@dbd:${servicedir}/slurmdbd.service"
+compute_login_nodes=("node0" "node1" "node2")
+for n in ${compute_login_nodes[@]}; do
+	scp "${servicedir}/slurmd.service" "vagrant@${n}:${servicedir}/slurmd.service"
+	#sudo echo SLURMD_OPTIONS=--conf-server=ctld:6817 > /etc/default/slurmd
+done
+'
