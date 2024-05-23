a3119aeca3e2b0a900500b210b9ac018b3b9c657
Marshall Garey <marshall@schedmd.com>
2024-05-23 17:09:01 -0600

    Update script to restart slurm daemons

--- ubuntunew/start_slurm.sh
+++ ubuntunew/start_slurm.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-nodes=("node0" "node1")
-nohup vagrant ssh dbd -c 'slurmdbd' &
-nohup vagrant ssh ctld -c 'slurmctld' &
-for n in $nodes
-do
-	nohup vagrant ssh $n -c 'slurmd' &
+# TODO: Restart sackd?
+ctld_dbd_nodes=("dbd" "ctld")
+compute_nodes=("node0" "node1" "node2")
+nohup vagrant ssh dbd -c 'sudo systemctl restart slurmdbd'
+nohup vagrant ssh ctld -c 'sudo systemctl restart slurmctld'
+for n in ${nodes[@]}; do
+	nohup vagrant ssh $n -c 'sudo systemctl start slurmd' &
 done
 wait
