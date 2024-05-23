9827c82bf34d4a3b4ccbbe35e759345ad935a3cc
Marshall Garey <marshall@schedmd.com>
2024-05-23 17:28:25 -0600

    Fix startup scripts - use correct variable name for compute nodes

--- ubuntunew/start_slurm.sh
+++ ubuntunew/start_slurm.sh
@@ -2,9 +2,9 @@
 # TODO: Restart sackd?
 ctld_dbd_nodes=("dbd" "ctld")
 compute_nodes=("node0" "node1" "node2")
-nohup vagrant ssh dbd -c 'sudo systemctl restart slurmdbd'
-nohup vagrant ssh ctld -c 'sudo systemctl restart slurmctld'
-for n in ${nodes[@]}; do
+nohup vagrant ssh dbd -c 'sudo systemctl restart slurmdbd' &
+nohup vagrant ssh ctld -c 'sudo systemctl restart slurmctld' &
+for n in ${compute_nodes[@]}; do
 	nohup vagrant ssh $n -c 'sudo systemctl restart slurmd' &
 done
 wait
