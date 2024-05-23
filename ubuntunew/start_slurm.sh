38e5f35a1e312a1528a2eea1f6c7c0a0f889e075
Marshall Garey <marshall@schedmd.com>
2024-05-23 17:14:16 -0600

    Restart slurmd, not just start slurmd

--- ubuntunew/start_slurm.sh
+++ ubuntunew/start_slurm.sh
@@ -5,6 +5,6 @@ compute_nodes=("node0" "node1" "node2")
 nohup vagrant ssh dbd -c 'sudo systemctl restart slurmdbd'
 nohup vagrant ssh ctld -c 'sudo systemctl restart slurmctld'
 for n in ${nodes[@]}; do
-	nohup vagrant ssh $n -c 'sudo systemctl start slurmd' &
+	nohup vagrant ssh $n -c 'sudo systemctl restart slurmd' &
 done
 wait
