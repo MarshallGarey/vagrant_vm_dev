c39ca43a23b48184a645caf8b9b7ef151f7dac26
Marshall Garey <marshall@schedmd.com>
2024-05-23 17:46:54 -0600

    Ensure that slurmctld and slurmdbd are up before starting slurmds

--- ubuntunew/start_slurm.sh
+++ ubuntunew/start_slurm.sh
@@ -4,6 +4,7 @@ ctld_dbd_nodes=("dbd" "ctld")
 compute_nodes=("node0" "node1" "node2")
 nohup vagrant ssh dbd -c 'sudo systemctl restart slurmdbd' &
 nohup vagrant ssh ctld -c 'sudo systemctl restart slurmctld' &
+wait
 for n in ${compute_nodes[@]}; do
 	nohup vagrant ssh $n -c 'sudo systemctl restart slurmd' &
 done
